e2c548f8f61cfd0fffdef08756f5038cc3650d55
silvaij <isaque.josias@gmail.com>
2020-09-28 10:54:58 -0300

    Inicio 28-09-2020 - 10:54

---
+++ src/main/java/br/com/casadocodigo/ApiCasaDoCodigo/ApiCasaDoCodigoApplication.java
@@ -0,0 +1,13 @@
+package br.com.casadocodigo.ApiCasaDoCodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCasaDoCodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCasaDoCodigoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/casadocodigo/ApiCasaDoCodigo/ApiCasaDoCodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.casadocodigo.ApiCasaDoCodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCasaDoCodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
